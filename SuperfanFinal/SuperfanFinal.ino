715f5ce09a641a21c39e75d8ca614f4a65ec0b72
Tanuj Sane <tsane@wpi.edu>
2015-12-01 23:55:55 -0500

    Added header comment

--- SuperfanFinal/SuperfanFinal.ino
+++ SuperfanFinal/SuperfanFinal.ino
@@ -1,4 +1,15 @@
-
+/* FILENAME: SuperfanFinal.ino
+ * 
+ * BRIEF: 
+ * 
+ * AUTHORS:
+ * @author Tanuj Sane
+ * @author Dominic Cupo
+ * @author Annie Hernandez
+ * @author Patrick Murphy
+ * 
+ * START DATE: Dec. 12, 2015
+ */
 void setup() {
   
 }
