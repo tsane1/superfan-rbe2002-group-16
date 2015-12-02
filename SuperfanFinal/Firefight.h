983573b4dbc5f7a341981ab2287babb0b6380700
Tanuj Sane <tsane@wpi.edu>
2015-12-02 13:37:53 -0500

    Modified .ino to use Firefight

--- SuperfanFinal/Firefight.h
+++ SuperfanFinal/Firefight.h
@@ -14,19 +14,14 @@
 #ifndef FIREFIGHT_H
 #define FIREFIGHT_H
 
-enum ScanStates {
-  FOUND_FIRE, SCANNING, NO_FIRE
-};
-enum FireStatus {
-  FLAME_ON, FLAME_OFF
-};
+#include "Arduino.h"
 
-class Fighter{
+class Fan{
   public:
-    Fighter(){} // default constructor
-    Fighter(int stepPin1, int stepPin2, int fanPin)
-    int sweep(); // Function to scan for fire using stepper
-    int extinguish(); // Function to turn on fan and blow out fire
+    Fan(){} // unused, default constructor
+    Fan(int stepPin1, int stepPin2, int fanPin)
+    boolean sweep(); // Function to scan for fire using stepper
+    boolean extinguish(); // Function to turn on fan and blow out fire
   private:
     int stepPin1, stepPin2, fanPin;
 };
--- SuperfanFinal/SuperfanFinal.ino
+++ SuperfanFinal/SuperfanFinal.ino
@@ -10,11 +10,17 @@
  * 
  * START DATE: Dec. 12, 2015
  */
- 
+
+#include "Firefight.h"
+
+boolean found, extinguished;
+Fan f(/*TODO: Choose pins*/);
+
 void setup() {
-  
+  found = f.scan();
 }
 
 void loop() {
+  if(!found) found = f.scan();
   
 }
