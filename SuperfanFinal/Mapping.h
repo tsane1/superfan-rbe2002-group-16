a71b9230c8e93919e7a68eadd2f75bf11e63d249
Tanuj Sane <tsane@wpi.edu>
2015-12-02 14:24:34 -0500

    Added Mapping.h file

---
+++ SuperfanFinal/Mapping.h
@@ -0,0 +1,12 @@
+/* FILENAME: Mapping.h
+ * 
+ * BRIEF: 
+ * 
+ * AUTHORS:
+ * @author Tanuj Sane
+ * @author Dominic Cupo
+ * @author Annie Hernandez
+ * @author Patrick Murphy
+ * 
+ * START DATE: Dec. 12, 2015
+ */
